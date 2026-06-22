f8f4a40fc9f3401ab35468bb8e7f8786d98d891d
Bhargav Kumar Nath <bhargavkumarnathh@gmail.com>
2026-06-23 00:27:37 +0100

    legacy project removal and readme updates

--- src/symbolr/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.symbolr.core.evaluator import BaseEvaluator
-from src.symbolr.core.bridge import RustEvolutionBridge, GenerationResult
-
-__all__ = ["BaseEvaluator", "RustEvolutionBridge", "GenerationResult"]
--- src/symbolr/evaluators/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.symbolr.evaluators.synthetic import SyntheticEvaluator
-from src.symbolr.evaluators.gradient_aware import GradientAwareEvaluator
-
-__all__ = ["SyntheticEvaluator", "GradientAwareEvaluator"]
